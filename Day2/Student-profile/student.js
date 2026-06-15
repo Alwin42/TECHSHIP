65133e40d8a5016273ffec77afb63101e342795f
Alwin42 <alwinemmanuel424@gmail.com>
2026-06-15 13:45:49 +0530

    Day 2 exercise

---
+++ Day2/Student-profile/student.js
@@ -0,0 +1,9 @@
+var name = 'Alwin Emmanuel';
+var college = 'AISAT Engineering College';
+var branch = 'Computer Scence and Engineering';
+var city = 'Ernakulam';
+
+document.getElementById("name").textContent = name;
+document.getElementById("College").textContent = college;
+document.getElementById("Branch").textContent = branch;
+document.getElementById("City").textContent = city;
